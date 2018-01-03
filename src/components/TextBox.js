8584a9539f3e91c49b366812b8099721f67be6f0
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-01-04 02:46:30 +0700

    :sparkles: :sparkles: TextBox

---
+++ src/components/TextBox.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const TextBox = () => (
+
+      <input/>
+
+);
+export default TextBox;
--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import TextBox from '../components/TextBox';
 const Dashboard = () => {
-
     return(
       <div>
         <form>
-          <input />
+        <h1>INSERT</h1>
+          <TextBox />
+        </form>
+        <form>
+        <h1>SEARCH</h1>
+          <TextBox/>
         </form>
       </div>
     )
