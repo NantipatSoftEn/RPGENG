6843e741903426f9d36802570d20edfe261d7be5
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-01-04 02:05:05 +0700

    :globe_with_meridians: :globe_with_meridians:DASH BOARD

---
+++ src/containers/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Dashboard = () => {
+
+    return(
+      <div>
+        <form>
+          <input />
+        </form>
+      </div>
+    )
+};
+
+export default Dashboard;
