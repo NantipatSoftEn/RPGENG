3ea89d3fe47822f7a6893677fedd2b89b17d6639
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-01-04 15:42:19 +0700

    :construction: :construction: :construction: TODO

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -31,6 +31,31 @@ const validate = (values) => {
   return errors;
 }
 
+const validate2 = (values) => {
+  const errors = {name:{}};
+  if(!values.name || !values.name.first) {
+    errors.name.first = {
+      message: 'You need to provide First Name'
+    }
+  }
+  if(!values.phone) {
+    errors.phone = {
+      message: 'You need to provide a Phone number'
+    }
+  }
+  
+  if(!values.email) {
+    errors.email = {
+      message: 'You need to provide an Email address'
+    }
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = {
+      message: 'Invalid email address'
+    }
+  }
+  return errors;
+}
+
 class ContactForm extends Component {
 
   componentWillReceiveProps = (nextProps) => { // Load Contact Asynchronously
@@ -54,14 +79,15 @@ class ContactForm extends Component {
       <Grid centered columns={2}>
         <Grid.Column>
           <h1 style={{marginTop:"1em"}}>{contact._id ? 'Edit Contact' : 'Add New Contact'}</h1>
-
-
           <Form onSubmit={handleSubmit} loading={loading}>
             <Form.Group widths='equal'>
-              <Field name="vocabulary" type="text" component={this.renderField} label="Vocabulary"/>
+              <Field name="name.first" type="text" component={this.renderField} label="First Name"/>
             </Form.Group>
-            <textarea name="des"  component={this.renderField} />
-            <Field name="keyword" type="text" component={this.renderField} label="Keyword"/>
+            <Form.Group widths='equal'>
+                <textarea name="name.last" type="text" component={this.renderField} label="Last Name"/>
+            </Form.Group>
+            <Field name="phone" type="text" component={this.renderField} label="Phone"/>
+            <Field name="email" type="text" component={this.renderField} label="Email"/>
             <Button primary type='submit' disabled={pristine || submitting}>Save</Button>
           </Form>
         </Grid.Column>
