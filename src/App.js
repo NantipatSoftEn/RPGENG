67ee84b88fe9bd3d5ff59f0d428150fda3533be6
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-01-05 03:22:35 +0700

    :sparkles: :sparkles: home

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { NavLink, Route } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 import ContactListPage from './pages/contact-list-page';
 import ContactFormPage from './pages/contact-form-page';
-
+import Home from './pages/home';
 class App extends Component {
   render() {
     return (
       <Container>
-        <div className="ui two item menu">
+        <div className="ui three item menu">
+          <NavLink className="item" activeClassName="active" exact to="/contacts/home">﻿Ｈｏｍｅ</NavLink>
           <NavLink className="item" activeClassName="active" exact to="/">﻿Ｃｏｎｔａｃｔｓ  Ｌｉｓｔ</NavLink>
           <NavLink className="item" activeClassName="active" exact to="/contacts/new">﻿Ａｄｄ  Ｃｏｎｔａｃｔ</NavLink>
         </div>
         <Route exact path="/" component={ContactListPage}/>
+        <Route path="/contacts/home" component={Home}/>
         <Route path="/contacts/new" component={ContactFormPage}/>
         <Route path="/contacts/edit/:_id" component={ContactFormPage}/>
       </Container>
