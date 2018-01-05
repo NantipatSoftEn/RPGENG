2ed9a49929047ac25b79e701c441babd0d4bb2a0
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-01-05 14:54:10 +0700

    add img for icon bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { NavLink, Route } from 'react-router-dom';
-import { Container } from 'semantic-ui-react';
+import { Container,Image } from 'semantic-ui-react';
 import ContactListPage from './pages/contact-list-page';
 import ContactFormPage from './pages/contact-form-page';
 import Home from './pages/home';
@@ -9,9 +9,16 @@ class App extends Component {
     return (
       <Container>
         <div className="ui three item menu">
-          <NavLink className="item" activeClassName="active" exact to="/contacts/home">﻿Ｈｏｍｅ</NavLink>
-          <NavLink className="item" activeClassName="active" exact to="/">﻿Ｃｏｎｔａｃｔｓ  Ｌｉｓｔ</NavLink>
-          <NavLink className="item" activeClassName="active" exact to="/contacts/new">﻿Ａｄｄ  Ｃｏｎｔａｃｔ</NavLink>
+          <NavLink className="item" activeClassName="active" exact to="/contacts/home">
+          <Image  src='http://localhost:3000/Swashbuckler_Logo.png'width="25" height="25"/>
+            ﻿Ｈｏｍｅ</NavLink>
+          <NavLink className="item" activeClassName="active" exact to="/">
+          <Image  src='http://localhost:3000/0LX6b0P.png'width="25" height="25"/>
+          ﻿Ｃｏｎｔａｃｔｓ  Ｌｉｓｔ</NavLink>
+
+          <NavLink className="item" activeClassName="active" exact to="/contacts/new">
+          <Image  src='http://localhost:3000/Summoner_Logo.png'width="25" height="25"/>
+          ﻿Ａｄｄ  Ｃｏｎｔａｃｔ</NavLink>
         </div>
         <Route exact path="/" component={ContactListPage}/>
         <Route path="/contacts/home" component={Home}/>
